b4569023fb48d894024213d0c2ca117f7499da6c
Paulo Hercilio Araujo Santana <phasantana@sedurbs.se>
2023-07-06 16:55:27 -0300

    feature: Initing project

---
+++ src/test/java/me/kertf22/music_backend/MusicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kertf22.music_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
